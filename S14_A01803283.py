5941f9109215883d0cf722b0750a7b164779d892
a01803283-hub <a01803283@tec.mx>
2025-09-19 08:27:19 -0600

    Create S14_A01803283.py
    Hola profe está ante el mejor programador de la historia

---
+++ S14_A01803283.py
@@ -0,0 +1,20 @@
+#ejercicio 1
+edad = int(input("Ingresa tu edad: "))
+if edad >= 18:
+    print("Eres adulto.")
+else:
+    print("No eres adulto.")
+
+#ejercicio2
+
+#ejercicio 2
+edad = int(input("cual es tu edad: "))
+if 13 <= edad <= 19:
+    print("eres adolescente.")
+else:
+    print("no eres adolescente.")
+
+#ejerccio 3
+nacimiento = int(input("Ingresa tu año de nacimiento: "))
+edad = 2025 - nacimiento
+print("Tienes", edad, "años")
